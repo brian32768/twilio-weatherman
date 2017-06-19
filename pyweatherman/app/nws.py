e9ab971ac1bde647c4580be8c890a2762c952eec
Brian Wilson <brian@wildsong.biz>
2017-06-19 09:13:06 -0700

    rewrote to same format as geocode

--- pyweatherman/app/nws.py
+++ pyweatherman/app/nws.py
@@ -8,17 +8,27 @@ uri = "https://api.weather.gov/points/%s,%s/forecast"
 
 class nws(object):
 
-    def __init__(self, lat,lon):
-        r = requests.get(uri % (lat,lon))
-        j = json.loads(r.text)
-        properties = j["properties"]
-        self.updated = properties["updated"]
+    def __init__(self):
+        self.json = self.updated = self.detailedForecast = None
+        return
+
+    def fetch(self,latlon):
+        r = requests.get(uri % latlon)
+        self.json = json.loads(r.text)
+        return
+
+    def parse(self):
+        properties = self.json["properties"]
         periods = properties["periods"]
         current = periods[0]
+        self.updated = properties["updated"]
         self.detailedForecast = current["detailedForecast"]
+        return
 
 if __name__ == "__main__":
-    n = nws(38.352, -122.672)
+    n = nws()
+    n.fetch((38.352, -122.672))
+    n.parse()
     print("updated:", n.updated)
     print(n.detailedForecast)
 
