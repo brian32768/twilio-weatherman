5892e96bc89c03bcb525f79e68d703b61222f57c
Brian Wilson <brian@wildsong.biz>
2017-06-19 09:13:16 -0700

    added

---
+++ pyweatherman/app/geocode.py
@@ -0,0 +1,44 @@
+#
+#  Look up the lat,lon for a zip code.
+#
+import requests
+import json
+
+GOOGLE_API_KEY = 'AIzaSyB_92AooxM-aXprjHRQzvqrYU1_wSDhBx4'
+
+class geocode(object):
+
+    # Load sample so we can test without doing a fetch from Google.
+    json = {'status': 'OK', 'results': [{'address_components': [{'types': ['postal_code'], 'short_name': '94931', 'long_name': '94931'}, {'types': ['locality', 'political'], 'short_name': 'Cotati', 'long_name': 'Cotati'}, {'types': ['administrative_area_level_2', 'political'], 'short_name': 'Sonoma County', 'long_name': 'Sonoma County'}, {'types': ['administrative_area_level_1', 'political'], 'short_name': 'CA', 'long_name': 'California'}, {'types': ['country', 'political'], 'short_name': 'US', 'long_name': 'United States'}], 'formatted_address': 'Cotati, CA 94931, USA', 'types': ['postal_code'], 'geometry': {'viewport': {'southwest': {'lat': 38.299015, 'lng': -122.760056}, 'northeast': {'lat': 38.3539759, 'lng': -122.66663}}, 'location_type': 'APPROXIMATE', 'bounds': {'southwest': {'lat': 38.299015, 'lng': -122.760056}, 'northeast': {'lat': 38.3539759, 'lng': -122.66663}}, 'location': {'lat': 38.3272883, 'lng': -122.7237843}}, 'place_id': 'ChIJ0a2xlX9KhIAR6aHsyne6UrA'}]}
+    
+    def __init__(self):
+        self.updated = ''
+        self.detailedResponse = ''
+        return
+
+    def fetch(self, zip):
+        uri = "https://maps.googleapis.com/maps/api/geocode/json?key=%s&components=postal_code:%s" % (GOOGLE_API_KEY,zip)
+        #print(uri)
+        response = None
+        try:
+            r = requests.get(uri)
+            self.json = json.loads(r.text)
+        except Exception as e:
+            pass
+        return
+
+    def parse(self):
+        """Parse json from Google and return a (lat,lon) tuple """
+        geometry = ((self.json["results"])[0])["geometry"]
+        location = geometry["location"]
+        #print(location)
+        return (location["lat"],location["lng"])
+        
+if __name__ == "__main__":
+    n = geocode()
+    # comment next line to test parsing only
+    n.fetch("94931")
+    t = n.parse()
+    print(t)
+
+# That's all!
